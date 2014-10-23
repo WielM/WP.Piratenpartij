260f1394ee59556eb9c17ef3c5bcb95e2b7de6ff
WielM <WielM@users.noreply.github.com>
2014-10-24 00:54:09 +0200

    Update homepage_banner.php
    Ik heb de bovenste banner alvast weggehaald omdat de vacature al ingevuld is.
    Nu die plek nog vullen met de drie banners die door Steven zijn voorgesteld.

--- homepage_banner.php
+++ homepage_banner.php
@@ -1,12 +1,4 @@
 <div id="banner">
-	<div class="m1 clearfix">
-		<p class="first"><strong>Werken voor de Piratenpartij?</strong></p>
-		<p>Gezocht: Buitengewoon Commissielid
-			<span style="float:right; margin-bottom: 20px">
-				<a href="https://piratenpartij.nl/meedoen/201404-buitengewoon-commissielid/" class="btn btn-primary">meer informatie</a>
-			</span>
-		</p>
-	</div>
 	<div class="row-fluid lower">
 		<div class="span4 t2">
 			<p class="first centre" style="margin-top: 25px;"><strong>Bekijk ook</strong></p>
