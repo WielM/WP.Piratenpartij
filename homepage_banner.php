0c881efc8a2b776545ea72cee0e8600a2479f26a
WielM <wiel@forces-nl.org>
2014-10-25 03:58:01 +0200

    Banners
    Aanpassing voor extra drie banner in plaats van vacature banner

--- homepage_banner.php
+++ homepage_banner.php
@@ -1,18 +1,38 @@
 <div id="banner">
-	<div class="m1 clearfix">
-		<p class="first"><strong>Werken voor de Piratenpartij?</strong></p>
-		<p>Gezocht: Buitengewoon Commissielid
-			<span style="float:right; margin-bottom: 20px">
-				<a href="https://piratenpartij.nl/meedoen/201404-buitengewoon-commissielid/" class="btn btn-primary">meer informatie</a>
-			</span>
-		</p>
-	</div>
+  <div class="row-fluid upper">
+
+    <div class="span4 t2">
+      <p class="first centre" style="margin-top: 25px;"><strong>Bezoek het</strong></p>
+      <p>piratenweekend</p>
+      <p class="read-more">
+        <a href="/piratenweekend5/" class="btn btn-primary" title="Piratenweekend">Lees meer</a>
+      </p>
+    </div>
+
+    <div class="span4 t1 centre" style="background-color: #643794;">
+      <p class="first centre" style="margin-top: 25px;"><strong>Algemene</strong></p>
+      <p>Leden Vergadering 23-11</p>
+      <p class="read-more centre">
+        <a href="/alv2014-3/" class="btn btn-primary" title="ALV 23-11-2014">Lees meer</a>
+      </p>
+    </div>
+
+    <div class="span4 t1 last">
+      <p style="margin-top: 25px;"><strong>Stop</strong></p>
+      <p>TTIP!</p>
+      <p class="read-more">
+        <a href="site http://stop-ttip.org/" class="btn btn-primary" title="Ontvang onze nieuwsbrief">Lees meer</a>
+      </p>
+    </div>
+
+  </div>
 	<div class="row-fluid lower">
+
 		<div class="span4 t2">
 			<p class="first centre" style="margin-top: 25px;"><strong>Bekijk ook</strong></p>
 			<p>onze opiniestukken</p>
 			<p class="read-more">
-				<a href="https://opinie.piratenpartij.nl" class="btn btn-primary" title="Opinie">lees meer</a>
+				<a href="https://opinie.piratenpartij.nl" class="btn btn-primary" title="Opinie">Lees meer</a>
 			</p>
 		</div>
 
@@ -20,7 +40,7 @@
 			<p class="first centre" style="margin-top: 25px;"><strong>Waarom Piraten?</strong></p>
 			<p>Onze standpunten</p>
 			<p class="read-more centre">
-				<a href="/standpunten/" class="btn btn-primary" title="Onze standpunten">lees meer</a>
+				<a href="/standpunten/" class="btn btn-primary" title="Onze standpunten">Lees meer</a>
 			</p>
 		</div>
 
@@ -28,7 +48,7 @@
 			<p style="margin-top: 25px;"><strong>Ontvang onze nieuwsbrief</strong></p>
 			<p>direct in je inbox!</p>
 			<p class="read-more">
-				<a href="/nieuwsbrief/" class="btn btn-primary" title="Ontvang onze nieuwsbrief">abonneren</a>
+				<a href="/nieuwsbrief/" class="btn btn-primary" title="Ontvang onze nieuwsbrief">Abonneren</a>
 			</p>
 		</div>
 
